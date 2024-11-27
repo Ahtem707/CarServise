6f6f62bad4400da0712042e56081a238d6838256
Ahtem707 <sitzhalilov.a.i.2.19@gmail.com>
2024-11-27 21:42:21 +0300

    Исправление конфликтов

--- views/int2111/int/a2.js
+++ views/int2111/int/a2.js
@@ -1,8 +1,5 @@
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();
-<<<<<<< Updated upstream
-=======
 
     // Add your registration logic here
->>>>>>> Stashed changes
 });
